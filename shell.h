84d95738cb17f7540f2377a28836ca4164b9497c
bnbasilio <bnbasilio@gmail.com>
2020-04-13 21:47:08 +0000

    header file

---
+++ shell.h
@@ -0,0 +1,12 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#define MAX_LENGTH 1024
+#define BLANK_STRING " "
+
+int _strcmp(char *s1, char *s2);
+char *_strcpy(char *dest, char *src);
+unsigned int _strspn(char *s, char *accept);
+
