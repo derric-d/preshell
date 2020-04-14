464e90ab278e4c54581c39a5d74850ae4f91698d
bnbasilio <bnbasilio@gmail.com>
2020-04-14 21:09:01 +0000

    check

--- dollarprompt.c
+++ dollarprompt.c
@@ -15,7 +15,7 @@ void print_env(void)
 	if (!environ)
 		return;
 	for (i = 0; environ[i]; i++)
-		printf("%s\n", environ[i]);
+		printf("%s\n", environ[i]); /*TODO: change printf */
 }
 void eof_routine(char *line)
 {
@@ -90,10 +90,10 @@ int get_tokens(char *line, const char *delimiters, char ***argvp)
 		return (-1);
 	}
 	*argvp = NULL;
-	snew = line + strspn(line, delimiters);
+	snew = line + _strspn(line, delimiters);
 	if ((t = malloc(strlen(snew) + 1)) == NULL)
 		return (-1);
-	strcpy(t, snew);
+	_strcpy(t, snew);
 	if (strtok(t, delimiters) != NULL)
 		for (numtokens = 1; strtok(NULL, delimiters) != NULL; numtokens++)
 			;
@@ -109,7 +109,7 @@ int get_tokens(char *line, const char *delimiters, char ***argvp)
 		free(t);
 	else
 	{
-		strcpy(t, snew);
+		_strcpy(t, snew);
 		**argvp = strtok(t, delimiters);
 		for (i = 1; i < numtokens; i++)
 			*((*argvp) + i) = strtok(NULL, delimiters);
@@ -126,9 +126,9 @@ void shell_loop(void)
 	do {
 		printf("$ ");
 		line = read_line();
-		if (strcmp(line, "exit\n") == 0)
+		if (_strcmp(line, "exit\n") == 0)
 			break;
-		if (strcmp(line, "env\n") == 0)
+		if (_strcmp(line, "env\n") == 0)
 			print_env();
 		else
 			exarg(line);
